7f87fd88b5972dcefe05aca8596e2378b85a9474
Ahmad Septian <ahmadseptian@Ahmad.Septian>
2021-07-23 14:10:06 +0700

    update

--- app.js
+++ app.js
@@ -6,11 +6,28 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+const db = require('./app/models/')
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+        useFindAndModify: true
+    })
+
+    .then(() => {
+        console.log(`Database Connected`);
+    }).catch((err) => {
+        console.log(`Cannot connect to the Database`, err);
+        process.exit()
+    });
+
 app.get('/', (req, res) => {
     res.json({
         message: "Welcome to Rest Api Express"
     })
 })
+
+require('./app/routes/post.routes')(app)
 
 const PORT = 8000
 app.listen(PORT, () => {
